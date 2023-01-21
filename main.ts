6fc22e52f3e24eb087a326ab7646fffdf3d26125
Dean Bevan <dean.bevan@phoenix-web.org.uk>
2023-01-21 14:20:52 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,71 @@
+function Warmer () {
+    radio.sendString("Warmer")
+    for (let index = 0; index < 5; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    }
+}
 input.onButtonPressed(Button.A, function () {
     Colder()
 })
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "Colder") {
+        Colder()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    Warmer()
+})
 function Colder () {
-    basic.showLeds(`
-        . . . # .
-        . . # # #
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . # .
-        . . # # #
-        . # . # .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . # .
-        . . # # #
-        . . . . .
-        # . # . #
-        . . . . .
-        `)
+    radio.sendString("Colder")
+    for (let index = 0; index < 5; index++) {
+        basic.showLeds(`
+            . . . # .
+            . . # # #
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . # .
+            . . # # #
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . # .
+            . . # # #
+            . . . . .
+            # . # . #
+            . . . . .
+            `)
+    }
 }
+radio.setGroup(1)
 basic.forever(function () {
 	
 })
